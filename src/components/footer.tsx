791edfc3d9ebc95c88db692f0661b6e698866568
Sergii Mykhailov <sergii.mykhailov@gmail.com>
2021-11-15 23:41:43 +0100

    Added better date time formatting

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "@emotion/styled";
 
 import GitHubIcon from "../assets/github.svg";
+import LinkedInIcon from "../assets/linkedin.svg";
 import { graphql, useStaticQuery } from "gatsby";
 
 const Footer = () => {
@@ -10,16 +11,30 @@ const Footer = () => {
   return (
     <FooterContainer>
       <p>&copy; Sergii Mykhailov, {new Date().getFullYear()}</p>
-      <p className="actinable">
-        <a
-          href={data?.siteMetadata?.github?.url}
-          target="_blank"
-          rel="noreferrer"
-          title={data?.siteMetadata?.github?.description}
-        >
-          <GitHubIcon />
-        </a>
-      </p>
+
+      <div>
+        <p className="actionable">
+          <a
+            href={data?.siteMetadata?.linkedin?.url}
+            target="_blank"
+            rel="noreferrer"
+            title={data?.siteMetadata?.linkedin?.description}
+          >
+            <LinkedInIcon />
+          </a>
+        </p>
+
+        <p className="actionable">
+          <a
+            href={data?.siteMetadata?.github?.url}
+            target="_blank"
+            rel="noreferrer"
+            title={data?.siteMetadata?.github?.description}
+          >
+            <GitHubIcon />
+          </a>
+        </p>
+      </div>
     </FooterContainer>
   );
 };
@@ -29,6 +44,10 @@ const useFooterDataQuery = () => {
     query FooterData {
       site {
         siteMetadata {
+          linkedin {
+            url
+            description
+          }
           github {
             url
             description
@@ -51,7 +70,13 @@ const FooterContainer = styled.footer(props => ({
   paddingLeft: 6,
   paddingRight: 6,
 
-  "& > p": {
+  "& > div": {
+    display: "flex",
+    margin: 0,
+    paddingInline: "3px",
+  },
+
+  "& > p, & > div > p": {
     display: "flex",
     margin: 0,
     paddingInline: "3px",
@@ -66,7 +91,7 @@ const FooterContainer = styled.footer(props => ({
     padding: "3px 0",
   },
 
-  "& > p.actinable:hover": {
+  "& > div > p.actionable:hover": {
     backgroundColor: props.theme.colors.backgroundPrimaryActive,
     cursor: "pointer",
   },
