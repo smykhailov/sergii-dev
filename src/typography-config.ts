71478bdf86931d52914435d30d0c45e94f7bdb7e
Sergii Mykhailov <sergii.mykhailov@gmail.com>
2023-03-29 19:41:57 +0200

    Fixed text

--- src/typography-config.ts
+++ src/typography-config.ts
@@ -17,35 +17,6 @@ const typography = new Typography({
       marginBottom: rhythm(0),
     },
 
-    // blockquote: {
-    //   fontSize: "1.4em",
-    //   margin: "20px auto",
-    //   padding: "1.2em 30px 1.2em 75px",
-    //   borderLeft: "8px solid #78C0A8",
-    // },
-
-    // // blockquote::before{
-    // //   font-family:Arial;
-    // //   content: "\201C";
-    // //   color:#78C0A8;
-    // //   font-size:4em;
-    // //   position: absolute;
-    // //   left: 10px;
-    // //   top:-10px;
-    // // }
-
-    // // blockquote::after{
-    // //   content: '';
-    // // }
-
-    // // blockquote span{
-    // //   display:block;
-    // //   color:#333333;
-    // //   font-style: normal;
-    // //   font-weight: bold;
-    // //   margin-top:1em;
-    // // }
-
     options: {},
   }),
 });
